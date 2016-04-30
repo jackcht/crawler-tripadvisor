b4be5dfa1d1febf0366e0705a9d7f95632a97243
jackcht <chen0783@e.ntu.edu.sg>
2016-04-30 17:41:19 -0400

    reformat

--- src/com/persistence/AttractionDAO.java
+++ src/com/persistence/AttractionDAO.java
@@ -1,10 +1,4 @@
 package com.persistence;
-
-//import org.hibernate.HibernateException;
-//import org.hibernate.Session;
-//import org.hibernate.Transaction;
-//import org.hibernate.criterion.Restrictions;
-
 import com.model.Attraction;
 
 public class AttractionDAO extends DAO<Attraction>{
@@ -12,7 +6,4 @@ public class AttractionDAO extends DAO<Attraction>{
 	public AttractionDAO(){
 		super(Attraction.class);
 	}
-	
-	
-
 }
